e1d195c716950c61926935c0df1d410321174d06
JhanellDenis <36853206+JhanellDenis@users.noreply.github.com>
2021-09-03 17:44:29 +0300

    modified the nav bar component

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,10 @@
+import react,{Component} from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
---
+++ frontend/frontend/src/components/Navbar.js
@@ -0,0 +1,51 @@
+/*import React from "react";
+
+export default function Navbar(){
+  return(
+    <div className="navbar">
+      <div className="aside">aside</div>
+      <div className="header">header</div>
+    </div>
+  );
+}*/
+
+
+import React, { Component } from "react";
+import { MenuItems } from "./MenuItems";
+import "../styles/Navbar.css";
+
+const styles = {
+  header: {
+    backgroundColor: "blue"
+  }
+}
+
+export class Navbar extends Component {
+  state = { clicked: false }
+  
+
+  render() {
+    return (
+      <div className="NavbarItems">
+        <div className="menu-icon"></div>
+        <ul className="ul">
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <span>
+                  <a className={item.cName} href={item.url}>
+                    {item.title}
+                  </a>
+                </span>
+                <hr />
+              </li>
+            );
+          })}
+        </ul>
+        <div>bdbxhgfjhgvjhbkhgcvhkgcv</div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
