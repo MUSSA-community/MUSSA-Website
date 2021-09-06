0530e9617e58e834ea66a4a6831044ea6538bd1c
Edgar Ngeleso <66532491+edgarngeleso@users.noreply.github.com>
2021-09-06 14:50:22 +0300

    Update Content.js

--- frontend/frontend/src/components/Content.js
+++ frontend/frontend/src/components/Content.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react"
 import Banner from "./Banner"
 import Science from "./Science"
-import "../../styles/Content.css"
+import "../styles/Content.css"
 
 export default function Content(){
     return(
